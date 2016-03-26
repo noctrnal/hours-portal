f3070f7d5c31251d08c38078e7a392ebae82af73
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-03-25 21:53:15 -0500

    Spacing

--- spec/models/sheet_spec.rb
+++ spec/models/sheet_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Sheet, :type => :model do
   describe "db structure" do
     it { is_expected.to have_db_column(:name).of_type(:string) }
   end
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
   end
